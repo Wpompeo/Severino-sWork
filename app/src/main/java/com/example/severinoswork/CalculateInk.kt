dfb57c66a9e761ef52d45070a856bfdad2f42840
Wpompeo <wag_pompeo@yahoo.com.br>
2024-12-21 23:03:45 -0300

    commit development "development of activities and layouts and spinner activity_calculate_ink.xml,Calculateink, Painting,  "

--- app/src/main/java/com/example/severinoswork/CalculateInk.kt
+++ app/src/main/java/com/example/severinoswork/CalculateInk.kt
@@ -1,6 +1,8 @@
 package com.example.severinoswork
 
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.severinoswork.databinding.ActivityCalculateInkBinding
 
@@ -43,17 +45,32 @@ class CalculateInk : AppCompatActivity() {
         binding.btnCalcInk.setOnClickListener {
             val resultWallCeiling = intent.getFloatExtra(KEY_RESULT_WALL_CEILING, 0f)
             val resultWall = intent.getFloatExtra(KEY_RESULT_WALL, 0f)
+            val resultFinal = findViewById<TextView>(R.id.tv_calc_final)
 
             if (resultWallCeiling > 0) {
 
                 val paintYieldCeiling = inkSize / 2
                 val finalWall = resultWallCeiling / paintYieldCeiling
+                val resultFinalFormat = "%.2f Latas de tinta".format(finalWall) + " para pintar $resultWallCeiling m² com duas de mãos. "
+                resultFinal.text = "Você precisará de $resultFinalFormat"
 
             } else {
                 val paintYield = inkSize / 2
                 val finalWall = resultWall / paintYield
+                val resultFinalFormat = "%.2f Latas de tinta".format(finalWall) + " para pintar $resultWall m² com duas de mãos."
+
+                resultFinal.text = "Você precisará de $resultFinalFormat"
 
             }
+        }
+        binding.btnFinishInk.setOnClickListener {
+            Toast.makeText(
+                this, "Finalizando aplicativo",
+                Toast.LENGTH_LONG
+            ).show()
+            binding.btnFinishInk.postDelayed({
+                finishAffinity()
+            }, 200)
         }
     }
 }
--- app/src/main/java/com/example/severinoswork/Painting.kt
+++ app/src/main/java/com/example/severinoswork/Painting.kt
@@ -6,6 +6,7 @@ import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.Spinner
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -24,19 +25,41 @@ class Painting : AppCompatActivity() {
         val edtLengthLengthPiece =
             findViewById<TextInputEditText>(R.id.edt_length_of_the_wall_length)
         val edtWidthCeiling = findViewById<TextInputEditText>(R.id.edt_width_wall_ceiling)
-        val edtLengthCeiling = findViewById<TextInputEditText>(R.id.edt_length_wall_width)
+        val edtLengthCeiling = findViewById<TextInputEditText>(R.id.edt_length_wall_ceiling)
         val cbRoof = findViewById<CheckBox>(R.id.cb_roof)
         val btnNextTint = findViewById<Button>(R.id.btn_calculate_tint)
+        val spinnerWidth: Spinner = findViewById(R.id.spn_wall_width)
+        val spinnerLength: Spinner = findViewById(R.id.spn_wall_length)
+
+        //Carrega itens array definido no strings xml
+        val numbresWall = resources.getStringArray(R.array.quantityWall)
+
+        //Cria o adpater
+        val adapter = ArrayAdapter(
+            this, R.layout.spinner_item,
+            numbresWall
+        )
+
+        //Define o layout para o dropdown
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        //Associa o adapter ao spinner
+        spinnerWidth.adapter = adapter
+        spinnerLength.adapter = adapter
 
         btnNextTint.setOnClickListener {
+            //Recupera valores spinner
+            val spinnerWidthValue = spinnerWidth.selectedItem.toString().toFloatOrNull() ?: 0f
+            val spinnerLengthValue = spinnerLength.selectedItem.toString().toFloatOrNull() ?: 0f
+
             val widthPieceStr: String = edtWidthPiece.text.toString()
             val lengthPieceStr: String = edtLengthPiece.text.toString()
             val lengthWidthPieceStr: String = edtLengthWidthPiece.text.toString()
             val lengthLengthPieceStr: String = edtLengthLengthPiece.text.toString()
             val widthCeilingStr: String = edtWidthCeiling.text.toString()
             val lengthCeilingStr: String = edtLengthCeiling.text.toString()
-            val spinnerWidth: Spinner = findViewById(R.id.spn_wall_width)
-            val spinnerLength: Spinner = findViewById(R.id.spn_wall_length)
+
+
 
             if (cbRoof.isChecked) {
                 if (widthPieceStr == "" || lengthPieceStr == "" || lengthWidthPieceStr == "" || lengthLengthPieceStr == ""
@@ -46,6 +69,7 @@ class Painting : AppCompatActivity() {
                         this, "Preencha todos os campos para avançar!",
                         Toast.LENGTH_LONG
                     ).show()
+                    return@setOnClickListener
                 }
             } else {
                 if (widthPieceStr == "" || lengthPieceStr == "" || lengthWidthPieceStr == "" || lengthLengthPieceStr == ""
@@ -55,55 +79,37 @@ class Painting : AppCompatActivity() {
                         this, "Favor rever os campos, campo desnecessário preenchido!",
                         Toast.LENGTH_LONG
                     ).show()
+                    return@setOnClickListener
                 }
             }
-            //carrega itens array definido no strings xml
-            val numbresWall = resources.getStringArray(R.array.quantityWall)
 
-            //cria o adpater
-            val adapter = ArrayAdapter(
-                this, android.R.layout.simple_spinner_item,
-                numbresWall
-            )
-
-            //define o layout para o dropdown
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-            //associa o adapter ao spinner
-            spinnerWidth.adapter = adapter
-            spinnerLength.adapter = adapter
-
+            val widthPiece = widthPieceStr.toFloat()
+            val lengthPiece = lengthPieceStr.toFloat()
+            val lengthWidthPiece = lengthWidthPieceStr.toFloat()
+            val lengthLengthPiece = lengthLengthPieceStr.toFloat()
 
             if (cbRoof.isChecked) {
-                val widthPiece = widthPieceStr.toFloat()
-                val lengthPiece = lengthPieceStr.toFloat()
-                val lengthWidthPiece = lengthWidthPieceStr.toFloat()
-                val lengthLengthPiece = lengthLengthPieceStr.toFloat()
+
                 val widthCeiling = widthCeilingStr.toFloat()
                 val lengthCeiling = lengthCeilingStr.toFloat()
 
                 //Medidas da peça com teto
                 val areaWallPiece =
-                    (widthPiece * lengthPiece) + (lengthWidthPiece * lengthLengthPiece)
+                    (widthPiece * lengthPiece * spinnerWidthValue) + (lengthWidthPiece * lengthLengthPiece * spinnerLengthValue)
                 val areaCeilingPiece = widthCeiling * lengthCeiling
 
                 val areaTotal = areaWallPiece + areaCeilingPiece
                 val intent = Intent(this, CalculateInk::class.java)
                 intent.putExtra(KEY_RESULT_WALL_CEILING, areaTotal)
-
-
+                startActivity(intent)
             } else {
-                val widthPiece = widthPieceStr.toFloat()
-                val lengthPiece = lengthPieceStr.toFloat()
-                val lengthWidthPiece = lengthWidthPieceStr.toFloat()
-                val lengthLengthPiece = lengthLengthPieceStr.toFloat()
-
 
                 //Medidas da peça sem teto
                 val areaWallPiece =
-                    (widthPiece * lengthPiece) + (lengthWidthPiece * lengthLengthPiece)
+                    (widthPiece * lengthPiece * spinnerWidthValue) + (lengthWidthPiece * lengthLengthPiece * spinnerLengthValue)
                 val intent = Intent(this, CalculateInk::class.java)
                 intent.putExtra(KEY_RESULT_WALL, areaWallPiece)
+                startActivity(intent)
 
             }
         }
